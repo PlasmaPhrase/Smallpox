77121983acb60f4dab8dcb7569f8d97a9369fade
Sillyz-beep <aremmel14@gmail.com>
2026-02-02 17:34:44 -0500

    Update jokers.lua

--- src/jokers.lua
+++ src/jokers.lua
@@ -1,23 +1,33 @@
--- Example Joker
+-- Kasane Teto
 SMODS.Joker {
-    key = "examplejoker",
-    pos = { x = 0, y = 0 },
-    rarity = 1,
+    key = "teto",
     blueprint_compat = true,
-    cost = 2,
-    discovered = true,
-    config = { extra = { mult = 5 }, },
+    rarity = 3,
+    cost = 8,
+    pos = { x = 4, y = 0 },
+    discovered = false,
     atlas = 'CustomJokers',
 
+    config = { extra = { xmult = 1, xmult_gain = 0.5 } },
+
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
+        return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult } }
     end,
-    
+
     calculate = function(self, card, context)
+        if context.destroy_card then
+            if context.cardarea == G.play and context.destroy_card:is_suit("Hearts") and context.destroy_card:get_id() == 14 then
+                card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmult_gain
+                return { 
+                    remove = true,
+                    message = localize('k_upgrade_ex')
+                }
+            end
+        end
         if context.joker_main then
             return {
-                mult = card.ability.extra.mult
+                xmult = card.ability.extra.xmult
             }
         end
-    end
+    end,
 }
