a3bcffc06060e503b2595b37ae928a26018d8bff
Plasma <plasmaphrase@gmail.com>
2026-02-03 22:35:01 +0000

    Updated en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,5 +1,22 @@
 return {
     descriptions = {
+        Back = {
+            b_smallpox_showman = {
+                name = {
+                    "Showman Deck",
+                },
+                text = {
+                    "{C:attention}Joker{}, {C:tarot}Tarot{}, {C:planet}Planet{},",
+                    "and {C:spectral}Spectral{} cards may",
+                    "appear multiple times",
+                    "{s:0.8,C:inactive}Art by Inky",
+                },
+                unlock = {
+                    "{C:attention}Oh, this is the greatest show!",
+                    "{C:attention}Oh, this is the greatest show!",
+                }
+            },
+        },
         Joker = {
             j_smallpox_examplejoker = {
                 name = 'Joker+',
@@ -10,3 +27,4 @@ return {
         },
     }
 }
+
