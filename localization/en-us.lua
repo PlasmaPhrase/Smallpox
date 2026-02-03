a2cab55c3b00eb8ae09575f0a9a71b84d07b47c0
soulware1 <slop@example.com>
2026-02-02 20:54:48 -0600

    Ok smart guy, let's see YOU ***EAT THE GRINCH**

---
+++ localization/en-us.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        Joker = {
+            j_smallpox_grinch = {
+                name = 'The Grinch',
+                text = {
+                   {
+                    "{X:mult,C:white}+#1#{} to all {C:mult}+Mult{}",
+                    "{X:mult,C:white}+#2#{} to all {X:mult,C:white}XMult{}",
+                    "{X:mult,C:white}+(#2#/N){} to all {C:attention}higher-operation{} Mult",
+                    "{C:inactive,s:0.9}N being 10^ the used operation{}"
+                   },
+                   {
+                    "{X:mult,C:white}-#3#{} {C:attention}Base{} per round played",
+                    "{C:inactive}(Base: #1#)"
+                   }
+                }
+            },
+        },
+    }
+}
