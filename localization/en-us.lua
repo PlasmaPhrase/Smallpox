2cea5e4154e36f0d0eab249662666629b46a3585
CodeRevo <devrimulasyurtsever00@gmail.com>
2026-02-05 23:47:46 +0300

    Revo's Joker

---
+++ localization/en-us.lua
@@ -0,0 +1,16 @@
+return {
+    descriptions = {
+        Joker = {
+            j_smallpox_scales_of_justice = {
+                name = {"Scales of Justice", "{s:0.8}Created by {C:purple,s:0.8}Revo"},
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult if the",
+                    "{C:attention}sum{} of the scored cards' ranks is even", 
+                    "Gives an {C:green}extra{} {X:mult,C:white}X#2#{} Mult",
+                    "if there are {C:attention}equal{} amounts of jokers",
+                    "to this card's left and right.",
+                }
+            }
+        }
+    }
+}
