182aa90e561591822be9f743c72b330138603e05
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2026-02-06 11:30:00 -0700

    works

--- main.lua
+++ main.lua
@@ -46,6 +46,7 @@ assert(SMODS.load_file("./src/jokers/typ0.lua"))() -- Blackjack by Typ0
 assert(SMODS.load_file("./src/jokers/tatsu.lua"))() -- Butterfly Effect by Tatsu, p: Typ0
 assert(SMODS.load_file("./src/jokers/inky.lua"))() -- Restaurant Menu by Inky, p: Typ0
 assert(SMODS.load_file("./src/jokers/smallpox.lua"))() -- Smallpox by Plasma
+assert(SMODS.load_file("./src/jokers/hunter.lua"))() -- Smallpox by Plasma
 
 -- Unsure what this does
 local NFS = require("nativefs")
